0e2b7e2c49556132cc3c5089dd42f7d5dfb7e23c
ginggonzalez <ginggonzalez3@gmail.com>
2021-03-27 12:32:06 -0700

    feat: Double blinky sample

--- projects/sample/l5_application/main.c
+++ projects/sample/l5_application/main.c
@@ -11,13 +11,16 @@ static void delay(uint32_t ms) {
 
 int main() {
   gpio__port_clock_enable(GPIO__PORT_C);
-  gpio__gpio_s gpio = gpio__configure_as_output(GPIO__PORT_C, 13);
+  gpio__gpio_s gpio_0 = gpio__configure_as_output(GPIO__PORT_C, 13);
+  gpio__gpio_s gpio_1 = gpio__configure_as_output(GPIO__PORT_C, 15);
 
   while (1) {
-    gpio__set(gpio);
-    delay(100000);
-    gpio__reset(gpio);
-    delay(100000);
+    gpio__set(gpio_1);
+    gpio__reset(gpio_0);
+    delay(1000000);
+    gpio__set(gpio_0);
+    gpio__reset(gpio_1);
+    delay(1000000);
   }
   return 0;
 }
