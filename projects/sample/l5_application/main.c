1405388afcd8ccc6608c28a1b85250d4f459deb1
ginggonzalez <ginggonzalez3@gmail.com>
2021-03-27 18:28:14 -0700

    debug: Add a crude delay_ms using a hardware timer
    This delay will not be optimized out by the compiler if the optimization
    flag is non-zero

--- projects/sample/l5_application/main.c
+++ projects/sample/l5_application/main.c
@@ -5,8 +5,21 @@
 
 #include "gpio.h"
 
-static void delay(uint32_t ms) {
-  while(ms--);
+static void crude_delay_ms(uint32_t ms) {
+  uint32_t count_up_to = TIM2->CNT + (ms * 1000);
+  while (TIM2->CNT < count_up_to);
+}
+
+static void timer__enable_counter()
+{
+  uint32_t timer_2_enable = (1UL << 0);
+  uint32_t start_counter = (1UL << 0);
+  uint32_t generate_update_event = (1UL << 0);
+
+  RCC->APB1ENR |= timer_2_enable;
+  TIM2->PSC = 41;
+  TIM2->CR1 |= start_counter;
+  TIM2->EGR |= generate_update_event; // Required for the prescaler value to take effect
 }
 
 int main() {
@@ -14,13 +27,11 @@ int main() {
   gpio__gpio_s gpio_0 = gpio__configure_as_output(GPIO__PORT_C, 13);
   gpio__gpio_s gpio_1 = gpio__configure_as_output(GPIO__PORT_C, 15);
 
+  timer__enable_counter();
+
   while (1) {
-    gpio__set(gpio_1);
-    gpio__reset(gpio_0);
-    delay(1000000);
-    gpio__set(gpio_0);
-    gpio__reset(gpio_1);
-    delay(1000000);
+    gpio__toggle(gpio_1);
+    crude_delay_ms(1000);
   }
   return 0;
 }
